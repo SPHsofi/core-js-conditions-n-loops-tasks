f6450fc0c780bc972c773fcda06b9931f61139c2
SPHsofi <Sofia_pash@mail.ru>
2024-01-08 21:15:17 +0300

    refactor: solved task (2. getMaxNumber)

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -38,8 +38,16 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let res;
+  if (a >= b && a >= c) {
+    res = a;
+  } else if (b >= a && b >= c) {
+    res = b;
+  } else {
+    res = c;
+  }
+  return res;
 }
 
 /**
